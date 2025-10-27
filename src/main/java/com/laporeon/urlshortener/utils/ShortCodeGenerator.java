c05df92933a51803eaa6ff27e7a3e3a453a609e6
laporeon <175569928+laporeon@users.noreply.github.com>
2025-10-27 16:18:46 -0300

    feat(utils): create method to generate short code using secure random

---
+++ src/main/java/com/laporeon/urlshortener/utils/ShortCodeGenerator.java
@@ -0,0 +1,23 @@
+package com.laporeon.urlshortener.utils;
+
+import org.springframework.stereotype.Component;
+
+import java.security.SecureRandom;
+
+@Component
+public class ShortCodeGenerator {
+
+    private StringBuilder stringBuilder;
+    private SecureRandom secureRandom = new SecureRandom();
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    private static final int SHORT_CODE_LENGTH = 7;
+
+    public String generateShortCode() {
+        StringBuilder stringBuilder = new StringBuilder(SHORT_CODE_LENGTH);
+        for (int i = 0; i < SHORT_CODE_LENGTH; i++) {
+            int idx = secureRandom.nextInt(CHARACTERS.length());
+            stringBuilder.append(CHARACTERS.charAt(idx));
+        }
+        return stringBuilder.toString();
+    }
+}
