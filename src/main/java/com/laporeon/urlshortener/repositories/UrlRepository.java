12033988845a41d79b43511b2280fd0dd5bcf18a
laporeon <175569928+laporeon@users.noreply.github.com>
2025-10-27 16:17:53 -0300

    feat(repositories): create url repository and add existsByShortCode method

---
+++ src/main/java/com/laporeon/urlshortener/repositories/UrlRepository.java
@@ -0,0 +1,10 @@
+package com.laporeon.urlshortener.repositories;
+
+import com.laporeon.urlshortener.entities.Url;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UrlRepository extends MongoRepository<Url, String> {
+    Boolean existsByShortCode(String shortCode);
+}
