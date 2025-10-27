fbcd3ff1d7a7616806b3c9436f5d8122dcbe6a1d
laporeon <175569928+laporeon@users.noreply.github.com>
2025-10-27 17:30:02 -0300

    refactor(mappers): remove unused variables

--- src/main/java/com/laporeon/urlshortener/mappers/UrlMapper.java
+++ src/main/java/com/laporeon/urlshortener/mappers/UrlMapper.java
@@ -3,8 +3,6 @@ package com.laporeon.urlshortener.mappers;
 import com.laporeon.urlshortener.dtos.request.UrlRequestDTO;
 import com.laporeon.urlshortener.dtos.response.UrlResponseDTO;
 import com.laporeon.urlshortener.entities.Url;
-import com.laporeon.urlshortener.repositories.UrlRepository;
-import com.laporeon.urlshortener.utils.ShortCodeGenerator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -13,9 +11,6 @@ import java.time.LocalDateTime;
 @Component
 @RequiredArgsConstructor
 public class UrlMapper {
-
-    private final ShortCodeGenerator codeGenerator;
-    private final UrlRepository urlRepository;
 
     public UrlResponseDTO toDTO(Url url) {
         return new UrlResponseDTO(
