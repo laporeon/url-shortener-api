c3111378a4c744659b7dee0694dee622c369c8c8
laporeon <175569928+laporeon@users.noreply.github.com>
2025-10-28 16:48:40 -0300

    feat(repositories): add findByShortCode method returning optional url in repository

--- src/main/java/com/laporeon/urlshortener/repositories/UrlRepository.java
+++ src/main/java/com/laporeon/urlshortener/repositories/UrlRepository.java
@@ -4,7 +4,11 @@ import com.laporeon.urlshortener.entities.Url;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UrlRepository extends MongoRepository<Url, String> {
     Boolean existsByShortCode(String shortCode);
+
+    Optional<Url> findByShortCode(String shortCode);
 }
