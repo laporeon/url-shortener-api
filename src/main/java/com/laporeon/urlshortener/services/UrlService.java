1d27efc3f5eb1d037b8b1ce00ea17857166052e0
laporeon <175569928+laporeon@users.noreply.github.com>
2025-10-28 15:12:34 -0300

    feat(services): inject HttpServletRequest and BaseUrlGenerator; clean UrlService logic and improve DTO mapping

--- src/main/java/com/laporeon/urlshortener/services/UrlService.java
+++ src/main/java/com/laporeon/urlshortener/services/UrlService.java
@@ -6,12 +6,16 @@ import com.laporeon.urlshortener.entities.Url;
 import com.laporeon.urlshortener.mappers.UrlMapper;
 import com.laporeon.urlshortener.repositories.UrlRepository;
 import com.laporeon.urlshortener.utils.ShortCodeGenerator;
+import com.laporeon.urlshortener.utils.BaseUrlGenerator;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UrlService {
@@ -19,16 +23,23 @@ public class UrlService {
     private final UrlRepository urlRepository;
     private final UrlMapper urlMapper;
     private final ShortCodeGenerator codeGenerator;
-    private String shortCode;
-
-    private LocalDateTime expirationDate = LocalDate.now().plusDays(1).atTime(23, 0);;
+    private final HttpServletRequest request;
+    private final BaseUrlGenerator baseUrlGenerator;
 
     public UrlResponseDTO shortenUrl(UrlRequestDTO dto) {
+        String shortCode;
+        LocalDateTime expirationDate = LocalDate.now().plusDays(1).atTime(23, 0);;
+
         do {
             shortCode = codeGenerator.generateShortCode();
         } while(urlRepository.existsByShortCode(shortCode));
 
+        String baseURL = baseUrlGenerator.generateBaseUrl(request);
+
         Url url = urlRepository.save(urlMapper.toEntity(dto, shortCode, expirationDate));
-        return urlMapper.toDTO(url);
+
+        log.info("Saved URL: {}", url);
+
+        return urlMapper.toDTO(url, baseURL);
     }
 }
