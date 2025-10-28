3f6802a97f52bd10b1b4019cbe398d77716fce4c
laporeon <175569928+laporeon@users.noreply.github.com>
2025-10-28 16:47:28 -0300

    feat(controllers): implement redirect endpoint for shortCode with logging

--- src/main/java/com/laporeon/urlshortener/controllers/UrlController.java
+++ src/main/java/com/laporeon/urlshortener/controllers/UrlController.java
@@ -2,16 +2,19 @@ package com.laporeon.urlshortener.controllers;
 
 import com.laporeon.urlshortener.dtos.request.UrlRequestDTO;
 import com.laporeon.urlshortener.dtos.response.UrlResponseDTO;
+import com.laporeon.urlshortener.entities.Url;
 import com.laporeon.urlshortener.services.UrlService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+
+@Slf4j
 @RestController
 @RequestMapping
 @RequiredArgsConstructor
@@ -23,5 +26,21 @@ public class UrlController {
     public ResponseEntity<UrlResponseDTO> generateShortUrl(@Valid @RequestBody UrlRequestDTO dto) {
         UrlResponseDTO urlResponseDTO = urlService.shortenUrl(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(urlResponseDTO);
+    }
+
+    @GetMapping("/{shortCode}")
+    public ResponseEntity<Void> redirect(@PathVariable("shortCode") String shortCode) {
+        log.info("Short Code requested: {}", shortCode);
+
+        Url url = urlService.findByShortCode(shortCode);
+
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.setLocation(URI.create(url.getOriginalUrl()));
+
+        log.info("Redirecting to URL: {}", url.getOriginalUrl());
+
+        return ResponseEntity.status(HttpStatus.MOVED_PERMANENTLY).headers(headers).build();
+
     }
 }
