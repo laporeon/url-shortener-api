28565893d9355d4ece46dbc787188377c97e2458
laporeon <175569928+laporeon@users.noreply.github.com>
2025-10-29 17:05:24 -0300

    chore(mappers): move date generator to mapper

--- src/main/java/com/laporeon/urlshortener/mappers/UrlMapper.java
+++ src/main/java/com/laporeon/urlshortener/mappers/UrlMapper.java
@@ -3,6 +3,7 @@ package com.laporeon.urlshortener.mappers;
 import com.laporeon.urlshortener.dtos.request.UrlRequestDTO;
 import com.laporeon.urlshortener.dtos.response.UrlResponseDTO;
 import com.laporeon.urlshortener.entities.Url;
+import com.laporeon.urlshortener.utils.ExpirationDateGenerator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -12,17 +13,21 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor
 public class UrlMapper {
 
+    private final ExpirationDateGenerator dateGenerator;
+
     public UrlResponseDTO toDTO(Url url, String baseURL) {
         String shortUrl = baseURL + "/" + url.getShortCode();
 
         return new UrlResponseDTO(shortUrl, url.getExpiresAt());
     }
 
-    public Url toEntity(UrlRequestDTO dto, String shortCode, LocalDateTime expirationDate) {
+    public Url toEntity(UrlRequestDTO dto, String shortCode) {
+        LocalDateTime expiresAt = dateGenerator.generateExpiresAt(dto.expirationDate());
+
         Url url = new Url();
         url.setOriginalUrl(dto.originalUrl());
         url.setShortCode(shortCode);
-        url.setExpiresAt(expirationDate);
+        url.setExpiresAt(expiresAt);
         return url;
     }
 
