13b582c67001c5952469bc7d40e7cee0b0236acc
laporeon <175569928+laporeon@users.noreply.github.com>
2025-10-29 17:06:03 -0300

    refactor(services): remove expirationDateGenerator and organize attributes

--- src/main/java/com/laporeon/urlshortener/services/UrlService.java
+++ src/main/java/com/laporeon/urlshortener/services/UrlService.java
@@ -13,31 +13,27 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class UrlService {
 
+    private final HttpServletRequest request;
     private final UrlRepository urlRepository;
     private final UrlMapper urlMapper;
     private final ShortCodeGenerator codeGenerator;
-    private final HttpServletRequest request;
     private final BaseUrlGenerator baseUrlGenerator;
+
 
     public UrlResponseDTO shortenUrl(UrlRequestDTO dto) {
         String shortCode;
-        LocalDateTime expirationDate = LocalDate.now().plusDays(1).atTime(23, 0);;
-
         do {
             shortCode = codeGenerator.generateShortCode();
         } while(urlRepository.existsByShortCode(shortCode));
 
         String baseURL = baseUrlGenerator.generateBaseUrl(request);
 
-        Url url = urlRepository.save(urlMapper.toEntity(dto, shortCode, expirationDate));
+        Url url = urlRepository.save(urlMapper.toEntity(dto, shortCode));
 
         log.info("Saved URL: {}", url);
 
