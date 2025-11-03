60377a3f513e8c6fc8e9d982fdd304d20afe5c24
laporeon <175569928+laporeon@users.noreply.github.com>
2025-11-03 13:15:20 -0300

    feat(entities): add builder annotation on URL Entity

--- src/main/java/com/laporeon/urlshortener/entities/Url.java
+++ src/main/java/com/laporeon/urlshortener/entities/Url.java
@@ -1,6 +1,7 @@
 package com.laporeon.urlshortener.entities;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 
 @Document(collection = "urls")
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Url {
