f656ca860768273fb8b09c83ff3af24622bc6946
laporeon <175569928+laporeon@users.noreply.github.com>
2025-11-03 13:16:27 -0300

    refactor(services): remove UrlMapper and build entity and DTO directly

--- src/main/java/com/laporeon/urlshortener/services/UrlService.java
+++ src/main/java/com/laporeon/urlshortener/services/UrlService.java
@@ -4,14 +4,16 @@ import com.laporeon.urlshortener.dtos.request.UrlRequestDTO;
 import com.laporeon.urlshortener.dtos.response.UrlResponseDTO;
 import com.laporeon.urlshortener.entities.Url;
 import com.laporeon.urlshortener.exceptions.ShortCodeNotFoundException;
-import com.laporeon.urlshortener.mappers.UrlMapper;
 import com.laporeon.urlshortener.repositories.UrlRepository;
+import com.laporeon.urlshortener.utils.ExpirationDateGenerator;
 import com.laporeon.urlshortener.utils.ShortCodeGenerator;
 import com.laporeon.urlshortener.utils.BaseUrlGenerator;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Slf4j
 @Service
@@ -20,9 +22,9 @@ public class UrlService {
 
     private final HttpServletRequest request;
     private final UrlRepository urlRepository;
-    private final UrlMapper urlMapper;
     private final ShortCodeGenerator codeGenerator;
     private final BaseUrlGenerator baseUrlGenerator;
+    private final ExpirationDateGenerator dateGenerator;
 
 
     public UrlResponseDTO shortenUrl(UrlRequestDTO dto) {
@@ -31,13 +33,25 @@ public class UrlService {
             shortCode = codeGenerator.generateShortCode();
         } while(urlRepository.existsByShortCode(shortCode));
 
+        LocalDateTime expiresAt = dateGenerator.generateExpiresAt(dto.expirationDate());
+
+        Url url = Url.builder()
+                     .shortCode(shortCode)
+                     .originalUrl(dto.originalUrl())
+                     .expiresAt(expiresAt)
+                     .build();
+
+        urlRepository.save(url);
+
+        log.info("Short code: {} generated for URL: {}", shortCode, url.getOriginalUrl());
+
         String baseURL = baseUrlGenerator.generateBaseUrl(request);
+        String shortUrl = baseURL + "/" + url.getShortCode();
 
-        Url url = urlRepository.save(urlMapper.toEntity(dto, shortCode));
-
-        log.info("Saved URL: {}", url);
-
-        return urlMapper.toDTO(url, baseURL);
+        return new UrlResponseDTO(
+                shortUrl,
+                url.getExpiresAt()
+        );
     }
 
     public Url findByShortCode(String shortCode) {
