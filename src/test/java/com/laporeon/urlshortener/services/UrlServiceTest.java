e5d1fb541060e9348da70c1e43fd80039750794c
laporeon <175569928+laporeon@users.noreply.github.com>
2025-11-03 14:45:34 -0300

    test(services): improve UrlService tests with clear display names and validations

--- src/test/java/com/laporeon/urlshortener/services/UrlServiceTest.java
+++ src/test/java/com/laporeon/urlshortener/services/UrlServiceTest.java
@@ -1,5 +1,6 @@
 package com.laporeon.urlshortener.services;
 
+import com.laporeon.urlshortener.dtos.request.UrlRequestDTO;
 import com.laporeon.urlshortener.dtos.response.UrlResponseDTO;
 import com.laporeon.urlshortener.entities.Url;
 import com.laporeon.urlshortener.exceptions.ShortCodeNotFoundException;
@@ -8,6 +9,7 @@ import com.laporeon.urlshortener.utils.BaseUrlGenerator;
 import com.laporeon.urlshortener.utils.ExpirationDateGenerator;
 import com.laporeon.urlshortener.utils.ShortCodeGenerator;
 import jakarta.servlet.http.HttpServletRequest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,15 +17,17 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Optional;
 
-import static com.laporeon.urlshortener.commom.Constants.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
+@DisplayName("UrlService Tests")
 public class UrlServiceTest {
 
     @Mock
@@ -44,19 +48,44 @@ public class UrlServiceTest {
     @InjectMocks
     private UrlService urlService;
 
+    private Url validUrlEntity;
+    private UrlRequestDTO validRequestDTO;
+    private UrlResponseDTO expectedResponseDTO;
+
+    private final String VALID_SHORT_CODE = "a1b2c3d";
+    private final String VALID_URL = "https://www.youtube.com";
+    private final LocalDate VALID_EXPIRATION_DATE = LocalDate.now().plusDays(1);
+    private final String BASE_URL = "http://localhost:8080/";
+
+    @BeforeEach
+    void setUp() {
+        validRequestDTO = new UrlRequestDTO(
+                VALID_URL,
+                VALID_EXPIRATION_DATE
+        );
+        validUrlEntity = Url.builder()
+                            .id("69026cdf278513d576c67059")
+                            .shortCode(VALID_SHORT_CODE)
+                            .originalUrl(validRequestDTO.originalUrl())
+                            .expiresAt(VALID_EXPIRATION_DATE.atTime(23,0,0))
+                            .createdAt(LocalDateTime.now())
+                            .build();
+        expectedResponseDTO = new UrlResponseDTO(BASE_URL + "/" + VALID_SHORT_CODE, validUrlEntity.getExpiresAt());
+    }
+
     @Test
-    @DisplayName("Should return UrlResponseDTO when shortening Url with valid request data")
-    void shortenUrl_ShouldReturnUrlResponseDTOWhenRequestDataIsValid() {
+    @DisplayName("Should shorten Url successfully when given valid request data")
+    void shouldShourtenUrlSuccessfullyWhenGivenRequestData() {
         when(codeGenerator.generateShortCode()).thenReturn(VALID_SHORT_CODE);
         when(baseUrlGenerator.generateBaseUrl(request)).thenReturn(BASE_URL);
-        when(dateGenerator.generateExpiresAt(eq(VALID_URL_REQUEST_DTO.expirationDate()))).thenReturn(SAVED_URL_ENTITY.getExpiresAt());
+        when(dateGenerator.generateExpiresAt(eq(validRequestDTO.expirationDate()))).thenReturn(validUrlEntity.getExpiresAt());
         when(urlRepository.existsByShortCode(VALID_SHORT_CODE)).thenReturn(false);
-        when(urlRepository.save(any(Url.class))).thenReturn(SAVED_URL_ENTITY);
+        when(urlRepository.save(any(Url.class))).thenReturn(validUrlEntity);
 
-        UrlResponseDTO sut = urlService.shortenUrl(VALID_URL_REQUEST_DTO);
+        UrlResponseDTO sut = urlService.shortenUrl(validRequestDTO);
 
-        assertThat(sut.shortUrl()).isEqualTo(URL_RESPONSE_DTO.shortUrl());
-        assertThat(sut.expiresAt()).isEqualTo(URL_RESPONSE_DTO.expiresAt());
+        assertThat(sut.shortUrl()).isEqualTo(expectedResponseDTO.shortUrl());
+        assertThat(sut.expiresAt()).isEqualTo(expectedResponseDTO.expiresAt());
 
         verify(urlRepository, times(1)).save(any(Url.class));
         verify(urlRepository, atLeastOnce()).existsByShortCode(VALID_SHORT_CODE);
@@ -64,26 +93,28 @@ public class UrlServiceTest {
 
     @Test
     @DisplayName("Should return Url entity when short code exists and has not expired")
-    void findByShortCode_ShouldReturnUrlEntityWhenShortCodeExistsAndIsNotExpired() {
-        when(urlRepository.findByShortCode(VALID_SHORT_CODE)).thenReturn(Optional.of(SAVED_URL_ENTITY));
+    void shouldReturnUrlEntityWhenShortCodeExistsAndIsNotExpired() {
+        when(urlRepository.findByShortCode(VALID_SHORT_CODE)).thenReturn(Optional.of(validUrlEntity));
 
         Url sut = urlService.findByShortCode(VALID_SHORT_CODE);
 
-        assertThat(sut.getId()).isEqualTo(SAVED_URL_ENTITY.getId());
-        assertThat(sut.getShortCode()).isEqualTo(SAVED_URL_ENTITY.getShortCode());
-        assertThat(sut.getExpiresAt()).isEqualTo(SAVED_URL_ENTITY.getExpiresAt());
+        assertThat(sut.getId()).isEqualTo(validUrlEntity.getId());
+        assertThat(sut.getShortCode()).isEqualTo(validUrlEntity.getShortCode());
+        assertThat(sut.getExpiresAt()).isEqualTo(validUrlEntity.getExpiresAt());
 
         verify(urlRepository, times(1)).findByShortCode(VALID_SHORT_CODE);
     }
 
     @Test
     @DisplayName("Should throw exception when short code does not exist or has expired")
-    void findByShortCode_ShouldThrowExceptionWhenShortCodeNotFoundOrHasExpired() {
-        when(urlRepository.findByShortCode(EXPIRED_SHORT_CODE)).thenReturn(Optional.empty());
+    void shouldThrowExceptionWhenShortCodeNotFoundOrHasExpired() {
+        String expiredShortCode = "expired";
 
-        assertThatThrownBy(() -> urlService.findByShortCode(EXPIRED_SHORT_CODE))
+        when(urlRepository.findByShortCode(expiredShortCode)).thenReturn(Optional.empty());
+
+        assertThatThrownBy(() -> urlService.findByShortCode(expiredShortCode))
                 .isInstanceOf(ShortCodeNotFoundException.class);
 
-        verify(urlRepository, times(1)).findByShortCode(EXPIRED_SHORT_CODE);
+        verify(urlRepository, times(1)).findByShortCode(expiredShortCode);
     }
 }
