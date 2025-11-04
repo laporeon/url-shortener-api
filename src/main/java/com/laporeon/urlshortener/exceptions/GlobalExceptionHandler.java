b41854d8456bad5b8e950b29242bb6239a1569bf
laporeon <175569928+laporeon@users.noreply.github.com>
2025-11-04 11:52:29 -0300

    fix(exceptions): remove unused imports at global exception handler

--- src/main/java/com/laporeon/urlshortener/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/laporeon/urlshortener/exceptions/GlobalExceptionHandler.java
@@ -3,14 +3,12 @@ package com.laporeon.urlshortener.exceptions;
 import com.laporeon.urlshortener.dtos.response.ErrorResponseDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.Instant;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @RestControllerAdvice
