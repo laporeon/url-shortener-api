af81391a8ce6e6846313984bdc7b7d7391bce554
laporeon <175569928+laporeon@users.noreply.github.com>
2025-11-05 18:43:14 -0300

    refactor(config): make Swagger servers dynamic by removing hardcoded server URL

--- src/main/java/com/laporeon/urlshortener/config/SwaggerConfiguration.java
+++ src/main/java/com/laporeon/urlshortener/config/SwaggerConfiguration.java
@@ -3,7 +3,6 @@ package com.laporeon.urlshortener.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,15 +11,10 @@ import java.util.Collections;
 @Configuration
 public class SwaggerConfiguration {
 
-    @Value("${server.port}")
-    private String serverPort;
 
     @Bean
     public OpenAPI openAPI() {
-        String serverUrl = "http://localhost:" + serverPort;
-
         return new OpenAPI()
-                .servers(Collections.singletonList(new Server().url(serverUrl)))
                 .info(
                         new Info()
                                 .title("URL Shortener API")
