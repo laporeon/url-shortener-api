549e94f13b7477e8f1f751641860f57da9a3b5a9
laporeon <175569928+laporeon@users.noreply.github.com>
2025-11-17 11:54:37 -0300

    feat(exceptions): implement handler for general exceptions on global exception handler

--- src/main/java/com/laporeon/urlshortener/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/laporeon/urlshortener/exceptions/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.laporeon.urlshortener.exceptions;
 
 import com.laporeon.urlshortener.dtos.response.ErrorResponseDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -11,22 +12,23 @@ import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponseDTO> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
-        Map<String, String> messages = new HashMap<>();
+        Map<String, String> details = new HashMap<>();
         ex.getBindingResult()
           .getFieldErrors()
           .forEach(fieldError ->
-                           messages.put(fieldError.getField(), fieldError.getDefaultMessage())
+                           details.put(fieldError.getField(), fieldError.getDefaultMessage())
           );
 
         ErrorResponseDTO error = buildErrorResponse(
                 HttpStatus.BAD_REQUEST,
                 "Validation Failed",
-                messages);
+                details);
 
         return ResponseEntity.badRequest().body(error);
     }
@@ -34,15 +36,27 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ShortCodeNotFoundException.class)
     public ResponseEntity<ErrorResponseDTO> handleShortCodeNotFoundException(ShortCodeNotFoundException ex) {
 
-        Map<String, String> messages = new HashMap<>();
-        messages.put("shortCode", ex.getMessage());
+        Map<String, String> details = new HashMap<>();
+        details.put("shortCode", ex.getMessage());
 
         ErrorResponseDTO error = buildErrorResponse(
                 HttpStatus.NOT_FOUND,
                 "Resource Not Found",
-                messages);
+                details);
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDTO> handleException(Exception ex) {
+        log.error("Unexpected error occurred", ex);
+
+        ErrorResponseDTO error = buildErrorResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                "An unexpected error occurred",
+                 null);
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
     }
 
     private ErrorResponseDTO buildErrorResponse(HttpStatus httpStatus, String title, Map<String, String> details) {
