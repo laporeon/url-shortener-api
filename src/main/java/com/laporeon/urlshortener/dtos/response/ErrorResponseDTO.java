128e7510fc33a56cb13bb066a4be54b10bb21c76
laporeon <175569928+laporeon@users.noreply.github.com>
2025-11-27 15:25:15 -0300

    chore(dtos): rename title to message and add JsonInclude to hide null fields

--- src/main/java/com/laporeon/urlshortener/dtos/response/ErrorResponseDTO.java
+++ src/main/java/com/laporeon/urlshortener/dtos/response/ErrorResponseDTO.java
@@ -1,11 +1,14 @@
 package com.laporeon.urlshortener.dtos.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.time.Instant;
 import java.util.Map;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record ErrorResponseDTO(int status,
                                String type,
-                               String title,
+                               String message,
                                Map<String, String> details,
                                Instant timestamp) {
 }
